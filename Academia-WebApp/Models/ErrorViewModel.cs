87fb175a441934826653415bf6ab14eced5b48f6
Guilherme dos santos <guisantosedex32@gmail.com>
2023-11-11 20:02:53 -0300

    Projeto Criado

---
+++ Academia-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Academia_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
